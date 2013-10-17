f20763db247ec3f2ccf6c30ae37c2e8bf65774e6
Tadashi Saito <tad.a.digger@gmail.com>
2013-10-17 21:30:01 +0900

    update endpoint with Heroku domain.

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 "use strict";
 
+var TRANSLATE_ENDPOINT = "//braille-ja.herokuapp.com/translate"
+
 $("#translate").click(function(ev) {
-    $.getJSON("/translate", {text: $("#original").val()}, function (obj) {
+    $.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}, function (obj) {
 	$("#translated").val(obj["translated"]);
     });
 });
