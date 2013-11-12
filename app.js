eb8e97a537e6a233a36f0b7840a7cdfa83f63548
Tadashi Saito <tad.a.digger@gmail.com>
2013-11-12 12:23:25 +0900

    add semicolon at eol.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var TRANSLATE_ENDPOINT = "http://braille-ja.herokuapp.com/translate"
-var PING_ENDPOINT = "http://braille-ja.herokuapp.com/ping"
-var BRAILLE_PATTERN = /[\u2800-\u28ff]+/g
+var TRANSLATE_ENDPOINT = "http://braille-ja.herokuapp.com/translate";
+var PING_ENDPOINT = "http://braille-ja.herokuapp.com/ping";
+var BRAILLE_PATTERN = /[\u2800-\u28ff]+/g;
 
 $("#translate").click(function(ev) {
     $.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}, function (obj) {
