20fe6e84dcc7e3b2fdc51b25b0227940e1d1c74a
Tadashi Saito <tad.a.digger@gmail.com>
2013-11-12 12:26:20 +0900

    set event hander after DOM setup.

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var TRANSLATE_ENDPOINT = "http://braille-ja.herokuapp.com/translate";
 var PING_ENDPOINT = "http://braille-ja.herokuapp.com/ping";
 var BRAILLE_PATTERN = /[\u2800-\u28ff]+/g;
 
-$("#translate").click(function(ev) {
-    $.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}, function (obj) {
-	var marked = obj["translated"].
-	    replace(BRAILLE_PATTERN, "<span class='braille-char'>$&</span>");
-	$("#translated").html(marked);
+$(function () {
+    $("#translate").click(function(ev) {
+	$.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}, function (obj) {
+	    var marked = obj["translated"].
+		replace(BRAILLE_PATTERN, "<span class='braille-char'>$&</span>");
+	    $("#translated").html(marked);
+	});
     });
 });
 
