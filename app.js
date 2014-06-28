62105c97413077974fac2e1718eb2a59e0cfd210
Tadashi Saito <tad.a.digger@gmail.com>
2014-06-28 10:07:31 +0900

    modernize usage of jQuery

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var BRAILLE_PATTERN = /[\u2800-\u28ff]+/g;
 
 $(function () {
     $("#translate").click(function(ev) {
-	$.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}, function (obj) {
-	    var marked = obj["translated"].
-		replace(BRAILLE_PATTERN, "<span class='braille-char'>$&</span>");
-	    $("#translated").html(marked);
-	});
+	$.getJSON(TRANSLATE_ENDPOINT, {text: $("#original").val()}).
+	    done(function (obj) {
+		var marked = obj["translated"].
+		    replace(BRAILLE_PATTERN, "<span class='braille-char'>$&</span>");
+		$("#translated").html(marked);
+	    });
     });
 });
 
-$.get(PING_ENDPOINT, null, function () {}, 'text');
+$.get(PING_ENDPOINT, 'text').
+    done(function() {});
